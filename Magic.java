77d4d9a6cd42adf68928c9e2d670c8725d982ecb
Jonas Achouri Sihlen <jonasasihlen@gmail.com>
2022-02-12 09:07:13 +0100

    Initial commit

---
+++ Magic.java
@@ -0,0 +1,17 @@
+public class Magic {
+    public static void main(String[] args) {
+
+        //Original number, can be set to any number
+        int myNumber = 999;
+
+        //Variables through compound assignment
+        int stepOne = myNumber * (myNumber);
+        int stepTwo = (stepOne) + (myNumber);
+        int stepThree = (stepTwo) / (myNumber);
+        int stepFour = (stepThree) + 17;
+        int stepFive = (stepFour) - (myNumber);
+        int stepSix = (stepFive) / 6;
+        System.out.println(stepSix);
+
+    }
+}
